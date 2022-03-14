4ec1599213e5abc553fee224034fb34f51783751
Brasidas <brasidas@meetmarvin.com>
2022-03-14 14:23:50 -0400

    create fresh react app, removed react filler html

---
+++ tictactoe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello World!
+      This is Mark Simon's first commit of his website.
+    </div>
+  );
+}
+
+export default App;
